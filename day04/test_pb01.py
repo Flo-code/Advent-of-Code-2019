a33f3e838e73fe5b38ad17c87e8f2e36efc8666c
Flo-code <florentv21@gmail.com>
2019-12-12 17:25:40 +0100

    rename test

--- day04/test_pb01.py
+++ day04/test_pb01.py
@@ -3,7 +3,7 @@ from pb01 import count_password
 
 
 class TestPb01Case(unittest.TestCase):
-    def TestPb01Case(self):
+    def test_case01(self):
         self.assertEqual(1, count_password(111111, 111111))
         self.assertEqual(0, count_password(223450, 223450))
         self.assertEqual(0, count_password(123789, 123789))
