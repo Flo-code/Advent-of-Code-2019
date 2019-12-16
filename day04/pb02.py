e3b82b8d4f07c66c4939f63969e0692f9f441a29
Flo-code <florentv21@gmail.com>
2019-12-16 18:03:43 +0100

    Edit day04

--- day04/pb02.py
+++ day04/pb02.py
@@ -1,5 +1,5 @@
-# this is a comment
-def counter_password(start, end):
+# Mon code
+"""def counter_password(start, end):
     nb_password = 0
 
     for number in range(start, end + 1):
@@ -16,8 +16,9 @@ def counter_password(start, end):
                     double = True
             previous.append(cpt)
 
-            if str(number).count(cpt) < 2:
-                no_triple = True
+           
+        if previous.count(cpt) < 2 :
+            no_triple = True
 
         if increasing and double and no_triple:
             nb_password += 1
@@ -26,4 +27,43 @@ def counter_password(start, end):
 
 
 counter_password(256310, 732736)
+"""
+# Internet
 
+
+def check_ascending(n):
+    return "".join(sorted(n)) == n
+
+
+def check_repeat(n):
+    for digit1, digit2 in zip(n[1:],n): 
+        if digit1 == digit2:
+            return True
+            
+
+
+def check_two_consecutive_digits(n):
+    repeat_count = 0
+    for n1, n2 in zip(n, n[1:]):
+        if n1 == n2:
+            repeat_count += 1
+        else:
+            if repeat_count == 1:
+                return True
+            repeat_count = 0
+    return repeat_count == 1
+
+
+p1_count = 0
+p2_count = 0
+
+for n in range(256310, 732736):
+    n = str(n)
+    if check_ascending(n):
+        if check_repeat(n):
+            p1_count += 1
+        if check_two_consecutive_digits(n):
+            p2_count += 1
+
+print(f"Part 1: {p1_count}")
+print(f"Part 2: {p2_count}")
